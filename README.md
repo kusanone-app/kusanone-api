dcb2b3ea2e99b31d7d6c073ffdbf23c30d3cb8b6
kusanone-app <citizen.media.reiwa@gmail.com>
2025-05-15 22:43:19 +0900

    fix: reset gitignore and re-add all files

--- README.md
+++
@@ -1,1 +0,0 @@
-# kusanone-api
--- routes/activity.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const activityController = require('../controllers/activityController');
-const { activitySchema, validateBody } = require('../middleware/validate');
-
-router.post('/register', validateBody(activitySchema), activityController.registerActivity);
-
-module.exports = router;
